2f6e014f7c1bb6e8703711ac5cc1630545337972
yilin0524 <43094334+yilin0524@users.noreply.github.com>
2018-12-04 01:31:53 -0500

    Create test2.java

---
+++ test/test2.java
@@ -0,0 +1,10 @@
+private DataPoint[] toDataPointArray(Date[] xAxis, double[] yAxis){
+        ArrayList<DataPoint> datalist = new ArrayList<DataPoint>();
+        for(int i = 0; i < xAxis.length; i++){
+            datalist.add(new DataPoint (xAxis[i], yAxis[i]));
+        }
+        DataPoint[] data = new DataPoint[datalist.size()];
+        data = datalist.toArray(data);
+        return data;
+    }
+    
